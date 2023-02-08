a4909fdaca79f649da294167acc84282d5628902
victoryagamii <vitor_almeida._@hotmail.com>
2023-02-08 00:16:33 -0300

    React

---
+++ src/mocks/cesta.js
@@ -0,0 +1,20 @@
+import logo from '../../assets/logo.png'
+
+
+const cesta = {
+    topo:{
+        titulo: "Detalhes da cesta",
+
+    },
+
+    detalhes:{
+        nome: "Cesta de Verduras",
+        logoFazenda: logo,
+        nomefazenda:"Jenny Jack Farm",
+        descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha.",
+        preco:"R$30,00"
+
+    }
+}
+
+export default cesta;
